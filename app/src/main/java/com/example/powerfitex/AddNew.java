91ee5d2b7dd7db4a5528a7a98bd35a0a24aec6b1
Lahiruchandika <lahiruchandika1@gmail.com>
2021-05-08 11:19:08 +0530

    Excercise Interfaces & Insert funtion

---
+++ app/src/main/java/com/example/powerfitex/AddNew.java
@@ -0,0 +1,84 @@
+package com.example.powerfitex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.TextUtilsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AddNew extends AppCompatActivity {
+
+    EditText  txtExID, txtExName, txtDetails;
+    Spinner txtBdyPrt, txtEq;
+    DatabaseReference dbRef;
+    Exercise Exr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new);
+
+        txtExID =findViewById(R.id.editTextID);
+        txtBdyPrt =findViewById(R.id.spinnerBPart);
+        txtExName =findViewById(R.id.editTextName);
+        txtEq =findViewById(R.id.spinnerEquipment);
+        txtDetails =findViewById(R.id.editTextDetails);
+
+        Exr = new Exercise();
+
+    }
+
+    public void Back (View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void insert (View view){
+
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Exercise");
+
+            try {
+                if (TextUtils.isEmpty(txtExID.getText().toString()))
+                    Toast.makeText(getApplicationContext(), "please enter id", Toast.LENGTH_SHORT).show();
+                else if(TextUtils.isEmpty(txtExName.getText().toString()))
+                    Toast.makeText(getApplicationContext(), "please enter name", Toast.LENGTH_SHORT).show();
+                else {
+                    Exr.setExID(txtExID.getText().toString().trim());
+                    Exr.setBodyPart(txtBdyPrt.getSelectedItem().toString().trim());
+                    Exr.setExName(txtExName.getText().toString().trim());
+                    Exr.setEquipment(txtEq.getSelectedItem().toString().trim());
+                    Exr.setDetails(txtDetails.getText().toString().trim());
+
+                    dbRef.push().setValue(Exr);
+
+                    Toast.makeText(getApplicationContext(), "Successfully Inserted", Toast.LENGTH_SHORT).show();
+                    clearControls();
+                }
+
+        }
+        catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), "invalid" , Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    private void clearControls() {
+
+        txtExID.setText("");
+        txtBdyPrt.setSelection(0);
+        txtExName.setText("");
+        txtEq.setSelection(0);
+        txtDetails.setText("");
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/powerfitex/Exercise.java
@@ -0,0 +1,64 @@
+package com.example.powerfitex;
+
+public class Exercise {
+    private String exID;
+    private String bodyPart;
+    private String exName;
+    private String equipment;
+    private String details;
+
+    public Exercise() {
+    }
+
+    public Exercise(String exID, String bodyPart, String exName, String equipment, String details) {
+        this.exID = exID;
+        this.bodyPart = bodyPart;
+        this.exName = exName;
+        this.equipment = equipment;
+        this.details = details;
+    }
+
+    public String getExID() {
+        return exID;
+    }
+
+    public void setExID(String exID) {
+        this.exID = exID;
+    }
+
+    public String getBodyPart() {
+        return bodyPart;
+    }
+
+    public void setBodyPart(String bodyPart) {
+        this.bodyPart = bodyPart;
+    }
+
+    public String getExName() {
+        return exName;
+    }
+
+    public void setExName(String exName) {
+        this.exName = exName;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String allExercices(){
+        return this.exID + "\n" + "Name : " + exName + "\n" + "Body Part : " + bodyPart;
+    }
+}
