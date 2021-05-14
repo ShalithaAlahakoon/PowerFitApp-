4e5aff134836ebd59f6e18ce39ecea0c66910174
PradeepBasnayake <basnayakeps1@gmail.com>
2021-05-14 12:26:09 +0530

    shedule delete

--- app/src/main/java/com/example/schedule_management/application_schedule.java
+++ app/src/main/java/com/example/schedule_management/application_schedule.java
@@ -3,12 +3,17 @@ package com.example.schedule_management;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class application_schedule extends AppCompatActivity {
+
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_schedule);
+
+
     }
 }
